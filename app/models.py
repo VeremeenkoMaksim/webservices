f4a53e7b4f286d776a203e8945df890cbea5ed5c
grialalaku <skyjackerz@icloud.com>
2021-04-22 13:10:41 +0900

    models

--- app/models.py
+++ app/models.py
@@ -5,12 +5,14 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-
-class Author(db.Model):
+class Student(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     last_name = db.Column(db.String(30), nullable=False)
     first_name = db.Column(db.String(30), nullable=False)
     second_name = db.Column(db.String(30))
+    stud_id = db.Column(db.Integer, nullable=False)
+    group = db.relationship('Group', backref='students', lazy=True)
+    group_id = db.Column(db.Integer, db.ForeignKey('group.id'))
 
     def __str__(self):
         result = f'{self.last_name} {self.first_name:.1}.'
@@ -19,31 +21,16 @@ class Author(db.Model):
         return result
 
     def to_dict(self):
-        return {'id': self.id, 'first_name': self.first_name, 'last_name': self.last_name, 'second_name': self.second_name}
+        return {'id': self.id, 'first_name': self.first_name, 'last_name': self.last_name, 'second_name': self.second_name, 'group': str(self.group)}
 
-
-class Book(db.Model):
+class Group(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), nullable=False)
-    author2 = db.relationship('Author', backref='books', lazy=True)
-    author2_id = db.Column(db.Integer, db.ForeignKey('author.id'))
+    name = db.Column(db.String(30), nullable=False)
+    year = db.Column(db.Integer, nullable=False)
+    status = db.Column(db.String(30), nullable=False)
 
     def __str__(self):
-        return self.title
+        return self.name
 
     def to_dict(self):
-        return {'id': self.id, 'title': self.title, 'author': str(self.author2)}
-
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(30), nullable=False)
-    password_salt = db.Column(db.String(16), nullable=False)
-    password_hash = db.Column(db.String(64), nullable=False)
-
-    def hash_password(self, password: str):
-        self.password_salt = os.urandom(16)
-        self.password_hash = scrypt(password.encode('utf-8'), salt=self.password_salt, n=16384, r=8, p=1)
-
-    def hash_verify(self, password: str):
-        return self.password_hash == scrypt(password.encode('utf-8'), salt=self.password_salt, n=16384, r=8, p=1)
+        return {'id': self.id, 'name': self.name, 'year': self.year, 'status':self.status }
