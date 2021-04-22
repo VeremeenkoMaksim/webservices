8dc3d61fa36e8b1e142028c6b0a62fd14c8a823a
grialalaku <skyjackerz@icloud.com>
2021-04-22 15:03:33 +0900

    index.html

--- app/__init__.py
+++ app/__init__.py
@@ -5,23 +5,12 @@ from flask_jwt_extended import JWTManager, create_access_token
 from flask_migrate import Migrate
 from flask_restful import Api
 
-from app.api import BooksResource, BookResource, AuthorsResource, AuthorResource
-from app.models import db, Author, Book, User
+from app.api import GroupsResource, GroupResource, StudentResource, StudentsResource
+from app.models import db, Student, Group
 
 
 def send_client(filename='index.html'):
     return send_from_directory('static', filename)
-
-
-def login():
-    username = request.json.get("username", None)
-    user = User.query.filter_by(username=username).first()
-    password = request.json.get("password", None)
-    if not user or not user.hash_verify(password):
-        return jsonify({"msg": "Bad username or password"}), 401
-    access_token = create_access_token(identity=username)
-    return jsonify(access_token=access_token)
-
 
 def create_app():
     app = Flask(__name__)
@@ -36,19 +25,18 @@ def create_app():
     Migrate(app, db, render_as_batch=('sqlite' in app.config['SQLALCHEMY_DATABASE_URI']))
 
     admin = Admin(app)
-    admin.add_view(ModelView(Author, db.session))
-    admin.add_view(ModelView(Book, db.session))
+    admin.add_view(ModelView(Student, db.session))
+    admin.add_view(ModelView(Group, db.session))
 
     JWTManager(app)
 
-    app.add_url_rule('/login/', view_func=login, methods=['POST'])
     app.add_url_rule('/', view_func=send_client)
     app.add_url_rule('/<path:filename>', view_func=send_client)
 
     api = Api(app)
-    api.add_resource(BooksResource, '/book/')
-    api.add_resource(BookResource, '/book/<int:book_id>/')
-    api.add_resource(AuthorsResource, '/author/')
-    api.add_resource(AuthorResource, '/author/<int:author_id>/')
+    api.add_resource(GroupsResource, '/group/')
+    api.add_resource(GroupResource, '/group/<int:group_id>/')
+    api.add_resource(StudentsResource, '/student/')
+    api.add_resource(StudentResource, '/author/<int:student_id>/')
 
     return app
